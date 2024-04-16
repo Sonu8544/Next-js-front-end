da0333a4e321655ab7ca9a63c84b31cfb470917e
Sonu Kumar <sksonu8544@gmail.com>
2024-04-17 00:20:19 +0530

    Fix name

--- src/components/StickyScroll.tsx
+++ src/components/StickyScroll.tsx
@@ -20,13 +20,7 @@ const content = [
             "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
         content: (
             <div className="h-full w-full  flex items-center justify-center text-white">
-                <Image
-                    src="/linear.webp"
-                    width={300}
-                    height={300}
-                    className="h-full w-full object-cover"
-                    alt="linear board demo"
-                />
+               <h1>Hello Sonu</h1>
             </div>
         ),
     },
