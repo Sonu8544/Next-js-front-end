9fae69f41aa6eb6367428e3b81a35ae4362522cd
Sonu Kumar <sksonu8544@gmail.com>
2024-04-17 01:43:10 +0530

    DEV background line

--- src/components/TestomonialCard.tsx
+++ src/components/TestomonialCard.tsx
@@ -1,5 +1,5 @@
 "use client";
- 
+
 import React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
@@ -7,43 +7,46 @@ export default function TestomonialCard() {
 
     const testimonials = [
         {
-          quote:
-            "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-          name: "Charles Dickens",
-          title: "A Tale of Two Cities",
+            quote:
+                "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+            name: "Charles Dickens",
+            title: "A Tale of Two Cities",
         },
         {
-          quote:
-            "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-          name: "William Shakespeare",
-          title: "Hamlet",
+            quote:
+                "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+            name: "William Shakespeare",
+            title: "Hamlet",
         },
         {
-          quote: "All that we see or seem is but a dream within a dream.",
-          name: "Edgar Allan Poe",
-          title: "A Dream Within a Dream",
+            quote: "All that we see or seem is but a dream within a dream.",
+            name: "Edgar Allan Poe",
+            title: "A Dream Within a Dream",
         },
         {
-          quote:
-            "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-          name: "Jane Austen",
-          title: "Pride and Prejudice",
+            quote:
+                "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+            name: "Jane Austen",
+            title: "Pride and Prejudice",
         },
         {
-          quote:
-            "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-          name: "Herman Melville",
-          title: "Moby-Dick",
+            quote:
+                "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+            name: "Herman Melville",
+            title: "Moby-Dick",
         },
-      ];
+    ];
 
-  return (
-    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="left"
-        speed="normal"
-      />
-    </div>
-  )
+    return (
+        <div className="dark:bg-grid-white/[0.2] bg-grid-black/[0.2] h-[30rem] rounded-md flex flex-col antialiased bg-white dark:bg-black items-center justify-center relative overflow-hidden">
+            <div>
+                <h2 className="text-center text-4xl uppercase" > sonu kumar </h2>
+                <InfiniteMovingCards
+                    items={testimonials}
+                    direction="left"
+                    speed="normal"
+                />
+            </div>
+        </div>
+    )
 }
